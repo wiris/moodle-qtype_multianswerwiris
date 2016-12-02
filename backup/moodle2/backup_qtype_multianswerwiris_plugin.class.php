97ace7804ba179dee4e40a3efdc4b0c28ac95a28
AndresMelo <Andres.Torres@blackboard.com>
2016-12-02 13:58:33 -0500

    Suggestions - code review, remove unused code
    unused code was removed

--- backup/moodle2/backup_qtype_multianswerwiris_plugin.class.php
+++ backup/moodle2/backup_qtype_multianswerwiris_plugin.class.php
@@ -25,37 +25,6 @@ class backup_qtype_multianswerwiris_plugin extends backup_qtype_multianswer_plug
     /**
      * Returns the qtype information to attach to question element
      */
-    /*protected function define_question_plugin_structure() {
-        // Define the virtual plugin element with the condition to fulfill
-        $plugin = $this->get_plugin_element(null, '../../qtype', 'multianswerwiris');
-
-        // Create one standard named plugin element (the visible container)
-        $pluginwrapper = new backup_nested_element($this->get_recommended_name());
-
-        // connect the visible container ASAP
-        $plugin->add_child($pluginwrapper);
-
-        // This qtype uses standard question_answers, add them here
-        // to the tree before any other information that will use them
-        $this->add_question_question_answers($pluginwrapper);
-
-        // Now create the qtype own structures
-        $multianswer = new backup_nested_element('multianswer', array('id'), array(
-            'question', 'sequence'));
-        $questionxml = new backup_nested_element('question_xml', array('id'), array('xml'));
-
-        // Now the own qtype tree
-        $pluginwrapper->add_child($multianswer);
-        $pluginwrapper->add_child($question_xml);
-
-        // set source to populate the data
-        $multianswer->set_source_table('question_multianswer',
-                array('question' => backup::VAR_PARENTID));
-        $question_xml->set_source_table('qtype_wq', array('question' => backup::VAR_PARENTID));
-
-        return $plugin;
-    }*/
-
     protected function define_question_plugin_structure() {
         // Call parent.
         $plugin = parent::define_question_plugin_structure();
