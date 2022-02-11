46f6d171c6e10f6da26b9a99785d6038a959bcad
Marc Vila Tornamorell <marc.vila@wiris.com>
2022-02-11 09:36:37 +0100

    fix: MOODLE_INTERNAL check needed on behat helper

--- tests/helper.php
+++ tests/helper.php
@@ -22,6 +22,9 @@
  * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
 
+
+defined('MOODLE_INTERNAL') || die();
+
 global $CFG;
 require_once($CFG->dirroot . '/question/type/multianswerwiris/question.php');
 
