16e480970170e2438442203c58870424c126679d
Marc Vila Tornamorell <marc.vila@wiris.com>
2022-02-11 09:31:53 +0100

    fix: typos and MOODLE_INTERNAL check not needed

--- tests/helper.php
+++ tests/helper.php
@@ -22,9 +22,6 @@
  * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
 
-
-defined('MOODLE_INTERNAL') || die();
-
 global $CFG;
 require_once($CFG->dirroot . '/question/type/multianswerwiris/question.php');
 
