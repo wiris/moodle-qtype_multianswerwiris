23c1d3365a10b45600251baacbf6e0bb5855f17d
Pol Torrent i Soler <pol@wiris.com>
2026-04-07 10:38:14 +0200

    chore: change requires to 4.5.0

--- version.php
+++ version.php
@@ -17,7 +17,7 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->version = 2026021900;
-$plugin->requires = 2024100706; // Moodle 4.5.6.
+$plugin->requires = 2024100700; // Moodle 4.5.0.
 $plugin->release = '4.14.0';
 $plugin->maturity = MATURITY_STABLE;
 $plugin->component = 'qtype_multianswerwiris';
